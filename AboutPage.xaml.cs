00a38e0232e864246c0db344cbb586b271406ced
Gabrielle <gabriellevasconcelosoliveira@gmail.com>
2025-04-11 20:16:12 -0300

    Inclusao do projeto NotesApp

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace NotesApp;
+
+public partial class AboutPage : ContentPage
+{
+	
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+
+    private void PerfilButton_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+}
